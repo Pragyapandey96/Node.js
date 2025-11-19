af0e10ea7a63f807cc5fff4ceee28c5de9a48abf
PragyaPandey96 <ppragya651@gmail.com>
2025-11-19 17:28:02 +0530

    in

---
+++ first.js
@@ -0,0 +1,11 @@
+
+const math = require("crypto")
+
+
+// console.log(math.addFn(2, 5));
+// console.log(math.subFn(8, 5));
+
+console.log("Math Value is", math);
+
+
+console.log(result);
---
+++ math.js
@@ -0,0 +1,15 @@
+// function add(a, b){
+//     return a + b;
+// }
+
+// function subtract(a, b){
+//     return a - b;
+// }
+
+// module.exports = {
+//     addFn: add,
+//     subFn: subtract,
+// }
+
+exports.add = (a, b) => a + b;
+exports.sub = (a, b) => a - b;
